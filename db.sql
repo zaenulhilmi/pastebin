de50d455889197ba7ea85d700f8baf3c8cf56033
Zaenul Hilmi <zaenulhilmi@gmail.com>
2022-04-17 02:20:47 +0700

    updating db sql

--- db.sql
+++ db.sql
@@ -4,7 +4,7 @@ USE pastebin;
 
 CREATE TABLE contents (
   shortlink VARCHAR(60) PRIMARY KEY,
-  content TEXT NOT NULL,
+  text TEXT NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   expiry_in_minutes INTEGER NOT NULL DEFAULT 0
 );
